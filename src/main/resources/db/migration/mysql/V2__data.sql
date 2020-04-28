560adc80c889699954867b989d51c2e92d934d12
Daniel Klotz <dklotz@goc.realcore-it.de>
2020-04-28 18:53:31 +0200

    Added initial data to db

---
+++ src/main/resources/db/migration/mysql/V2__data.sql
@@ -0,0 +1,190 @@
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+    NULL,
+    'Apple MacBook Air (2020) 13.3" with Touch ID',
+    "Apple MacBook Air (2020) 13.3' with Touch ID - Space Grey (Intel Core i3 1.1GHz/256GB SSD/8GB RAM)",
+    'MACBOOKS',
+    'https://multimedia.bbycastatic.ca/multimedia/products/250x250/145/14517/14517652.jpg',
+     1299.99);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+    NULL,
+    'Apple MacBook Pro w/ Touch Bar 13.3',
+    "Apple MacBook Pro w/ Touch Bar 13.3' - Space Grey (Intel Core i5 1.4GHz/128GB SSD/8GB RAM) ",
+    'MACBOOKS',
+    'https://multimedia.bbycastatic.ca/multimedia/products/150x150/137/13792/13792707.jpg',
+     1699.99);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+    NULL,
+    'Apple MacBook Air (2020) 13.3" with Touch ID',
+    "Apple MacBook Air (2020) 13.3' w/ Touch ID - Space Grey (Intel Core i5 1.1GHz/512GB SSD/8GB RAM)",
+    'MACBOOKS',
+    'https://multimedia.bbycastatic.ca/multimedia/products/250x250/145/14517/14517656.jpg',
+     1699.99);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+    NULL,
+    'Apple MacBook Pro w/ Touch Bar 16',
+    "Apple MacBook Pro w/ Touch Bar 16' - Space Grey (Intel Core i7 2.6GHz/512GB SSD/16GB RAM)",
+    'MACBOOKS',
+    'https://multimedia.bbycastatic.ca/multimedia/products/250x250/143/14335/14335048.jpg',
+     2999.99);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+    NULL,
+    'Apple MacBook Air 13.3" MD760LL/A 6 2013',
+    "Apple MacBook Air 13.3' MD760LL/A (Intel Core i5 Dual-Core 1.3GHz / 128GB SSD / 4GB RAM) - Certified Refurbished",
+    'MACBOOKS',
+    'https://multimedia.bbycastatic.ca/multimedia/products/250x250/132/13231/13231441.jpg',
+     789.99);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+    NULL,
+    'Apple MacBook Pro with Touch Bar 13.3" - Silver',
+    " (Intel Core i5 1.4GHz/256GB SSD/8GB RAM) - French",
+    'MACBOOKS',
+    'https://multimedia.bbycastatic.ca/multimedia/products/150x150/137/13792/13792712.jpg',
+     1949.99);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+    NULL,
+    'Apple iPhone 11 Pro Max 256GB - Midnight Green',
+    "6.5-inch Super Retina XDR OLED display, Water- and dust-resistant (4 metres for up to 30 minutes, IP68), Triple-camera system with 12MP Ultra Wide, Wide and Telephoto cameras; Night mode, Portrait mode and 4K video up to 60fps",
+    'IPHONES',
+    'https://merchandising-assets.bestbuy.ca/bltc8653f66842bff7f/bltb972321aaf8c188a/5d1d5846106d2f5640263e36/bbym-evergreen-icon-iphonexsmax.jpg',
+     1249.99);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+     NULL,
+     'Apple iPhone 11 Pro Max 64GB - Gold',
+     "6.5-inch Super Retina XDR OLED display, Water- and dust-resistant, Triple-camera system with 12MP Ultra Wide, Wide and Telephoto cameras; Night mode, Portrait mode and 4K video up to 60fps",
+     'IPHONES',
+     'https://multimedia.bbycastatic.ca/multimedia/products/250x250/138/13897/13897308.jpg',
+      1149.99);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+     NULL,
+     'Apple iPhone XS 64GB - Space Grey',
+     "5.8-inch Super Retina All-Screen OLED Display, A12 Bionic chip with 8-core Neural Engine, Faster Face ID, Advanced 12MP Dual Camera system with Smart HDR",
+     'IPHONES',
+     'https://multimedia.bbycastatic.ca/multimedia/products/250x250/130/13016/13016086.jpg',
+      899.99);
+
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+     NULL,
+     'Apple iPhone 8 64GB - Space Grey',
+     "4.7-inch Retina HD display with True Tone, All-glass and aluminum design, water- and dust resistant, 12MP camera with OIS and 4K video up to 60fps, A11 Bionic, the most powerful and smartest chip in a smartphone",
+     'IPHONES',
+     'https://multimedia.bbycastatic.ca/multimedia/products/250x250/115/11556/11556804.jpg',
+      599.99);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+      NULL,
+     'Apple iPhone 11 64GB - Black',
+     "6.1-inch Liquid Retina HD LCD display, Water- and dust-resistant (2 metres for up to 30 minutes, IP68, Dual-camera system with 12MP Ultra Wide and Wide cameras; Night mode, Portrait mode and 4K video up to 60fps",
+     'IPHONES',
+     'https://multimedia.bbycastatic.ca/multimedia/products/250x250/138/13897/13897259.jpg',
+      1099.99);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+     NULL,
+     'Apple iPad mini 7.9" 64GB with Wi-Fi - Space Grey',
+     "7.9-inch Retina display, A12 Bionic chip, Touch ID, Up to 256GB storage",
+     'IPADS',
+     'https://multimedia.bbycastatic.ca/multimedia/products/250x250/134/13487/13487358.jpg',
+      529.99);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+     NULL,
+     'Apple iPad mini 2 7.9-inch (Late 2013) - Wi-Fi - 16GB - Silver',
+     "7.5mm and less than a pound, 7.9' Retina Display, A7 chip with 64-bit architecture, Built-in WiFi MIMO technology",
+     'IPADS',
+     'https://multimedia.bbycastatic.ca/multimedia/products/250x250/126/12602/12602349.jpg',
+      349.99);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+     NULL,
+     'Apple iPad mini 7.9" 256GB with Wi-Fi - Gold',
+     "7.9-inch Retina display, A12 Bionic chip, Touch ID, Up to 256GB storage",
+     'IPADS',
+     'https://multimedia.bbycastatic.ca/multimedia/products/250x250/134/13487/13487363.jpg',
+      719.99);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+     NULL,
+     'Apple iPad 10.2" 128GB with Wi-Fi (7th Generation) - Gold',
+     "10.2-inch Retina display, A10 Fusion chip, Touch ID fingerprint sensor, 8MP back camera, 1.2MP FaceTime HD front camera",
+     'IPADS',
+     'https://multimedia.bbycastatic.ca/multimedia/products/250x250/138/13893/13893712.jpg',
+      549.99);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+    NULL,
+    'Apple iPad Pro 11" 128GB with Wi-Fi (4th Generation) - Silver',
+    "11-inch Liquid Retina display with ProMotion, A12Z Bionic chip with 8 Core Graphics, 128GB of built-in storage, 4K video recording",
+    'IPADS',
+    'https://multimedia.bbycastatic.ca/multimedia/products/250x250/145/14517/14517984.jpg',
+    1049.99);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+    NULL,
+    'Apple Magic Mouse 2 (MLA02LL/A) - White',
+    "Redesigned, rechargeable, remarkable. That's Apple accessories for you, and the Magic Mouse 2 is no different.",
+    'ACCESSORIES',
+    'https://multimedia.bbycastatic.ca/multimedia/products/250x250/103/10398/10398045.jpg',
+    95.00);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+    NULL,
+    'Apple Lightning to HDMI/Lightning Digital AV Adapter (MD826AM/A)',
+    "View content from your compatible iPad, iPhone or iPod touch on any HDMI equipped display with this Lightning Digital AV adapter.",
+    'ACCESSORIES',
+    'https://multimedia.bbycastatic.ca/multimedia/products/250x250/103/10373/10373188.jpg',
+    64.00);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+    NULL,
+    'Apple 85W MagSafe 2 Power Adapter (MD506LL/A)',
+    "Designed for use with your MacBook Pro with Retina Display, this Apple 85W MagSafe 2 power adapter is a great replacement or secondary accessory for your laptop",
+    'ACCESSORIES',
+    'https://multimedia.bbycastatic.ca/multimedia/products/250x250/102/10210/10210557.jpg',
+    95.00);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+    NULL,
+    'Apple iPad Pencil - White',
+    "When using iPad, there may be moments when you want even greater precision. The Apple Pencil has been painstakingly designed to expand on the versatility of Multi-Touch",
+    'ACCESSORIES',
+    'https://multimedia.bbycastatic.ca/multimedia/products/250x250/132/13233/13233103.jpg',
+    129.00);
+
+INSERT INTO `product` (`id`, `name`, `description`, `category`, `main_image`, price)
+VALUES (
+    NULL,
+    'Apple Magic Trackpad 2 (MJ2R2LL/A) - White',
+    "The Apple Magic Trackpad 2 brings the power of the Force Touch trackpad to the desktop for the first time.",
+    'ACCESSORIES',
+    'https://multimedia.bbycastatic.ca/multimedia/products/250x250/103/10398/10398044.jpg',
+    149.99);
+
